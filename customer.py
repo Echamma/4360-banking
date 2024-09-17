d2c6c8be00c99df488ebb1014b7459afc737bc27
ipeters33 <isabella.peters33@gmail.com>
2024-09-16 20:04:41 -0500

    refactor parameters to follow snake case formatting

--- customer.py
+++ customer.py
@@ -8,17 +8,17 @@ class Customer():
         self.balance = int(balance)
         self.counter = 0
 
-    def deposit(self, amountOfMoney):
-        if(amountOfMoney > 2000 or amountOfMoney < 0 or self.counter >2):
+    def deposit(self, deposit_amount):
+        if(deposit_amount > 2000 or deposit_amount < 0 or self.counter > 2):
             print("Invalid Transaction")
         else:
-            self.balance += int(amountOfMoney)
+            self.balance += int(deposit_amount)
 
-    def withdraw(self, amountOfMoney):
-        if(amountOfMoney > self.balance or amountOfMoney < 0):
+    def withdraw(self, withdraw_amount):
+        if(withdraw_amount > self.balance or withdraw_amount < 0):
             return "Invalid Transaction"        
         else:
-            self.balance -= int(amountOfMoney)
+            self.balance -= int(withdraw_amount)
 
     def to_json(self):
 
